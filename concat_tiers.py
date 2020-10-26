45bf35bc93b04ff816aebd6ea46ea1dc6f28dae7
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2020-10-26 17:09:54 -0500

    cleaned up Python script to meet standards

--- concat_tiers.py
+++ concat_tiers.py
@@ -1,56 +1,58 @@
-# Eli headers
-#f	x	y	pix	wt	md	dim	edge
-# frame
-# x coord
-# y coord
-# pix (radius?)
-# wt (tier)
-# md (moon diameter)
-# dim (box dimensions)
-# edge (is the moon on an edge?)
+"""
+This script concats the individual tiered data from frame_extraction into a single file
+"""
 
+
+import argparse
 import configparser
 import numpy as np
 
-with open("settings.cfg") as f:
-    file_content = '[dummy_section]\n' + f.read()
+def parse_settings():
+	'''
+	Parser allows for custom settings file locations
+	returns settings string
+	'''
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-c", "--config", action="store_true", default="settings.cfg", \
+		help="config file location")
+	args = parser.parse_args()
+	settings_location = args.config
+	return settings_location
 
-config_parser = configparser.RawConfigParser()
-config_parser.read_string(file_content)
+def main():
+	'''
+	Executes concatination script
+	'''
+	# The config parser requires sections which aren't compatible with the C++ cfg file.
+	# We insert a dummy section to bypass this restriction
+	with open(parse_settings()) as fff:
+		file_content = '[dummy_section]\n' + fff.read()
+	config_parser = configparser.RawConfigParser()
+	config_parser.read_string(file_content)
+	# Fetch output directory based on config
+	outdir = config_parser["dummy_section"]["outputdir"]
+	# Construct path to data files and output
+	tier_1 = "." + outdir + "data/Tier1.csv"
+	tier_2 = "." + outdir + "data/Tier2.csv"
+	tier_3 = "." + outdir + "data/Tier3.csv"
+	tier_4 = "." + outdir + "data/Tier4.csv"
+	output = "." + outdir + "data/mixed_tiers.csv"
+	# Open relevant files
+	frame_t1 = np.loadtxt(tier_1, delimiter=',', skiprows=1)
+	frame_t2 = np.loadtxt(tier_2, delimiter=',', skiprows=1)
+	frame_t3 = np.loadtxt(tier_3, delimiter=',', skiprows=1)
+	frame_t4 = np.loadtxt(tier_4, delimiter=',', skiprows=1)
+	# Concat and label with tier number
+	frame_t1 = np.column_stack((frame_t1, np.full(np.shape(frame_t1)[0], 1)))
+	frame_t2 = np.column_stack((frame_t2, np.full(np.shape(frame_t2)[0], 2)))
+	frame_t3 = np.column_stack((frame_t3, np.full(np.shape(frame_t3)[0], 3)))
+	frame_t4 = np.column_stack((frame_t4, np.full(np.shape(frame_t4)[0], 4)))
+	# Stack the tiers
+	frame_all = np.vstack((frame_t1, frame_t2, frame_t3, frame_t4))
+	# Sort based on the frame number
+	frame_all = frame_all[frame_all[:, 0].argsort()]
+	# Save
+	np.savetxt(output, frame_all, delimiter=',', fmt=['%d', '%f', '%f', '%f', '%d'])
+	return
 
-OUTDIR = config_parser["dummy_section"]["outputdir"]
-
-ELLS = "." + OUTDIR + "data/ellipses.csv"
-TIER_1 = "." + OUTDIR + "data/Tier1.csv"
-TIER_2 = "." + OUTDIR + "data/Tier2.csv"
-TIER_3 = "." + OUTDIR + "data/Tier3.csv"
-TIER_4 = "." + OUTDIR + "data/Tier4.csv"
-OUTPUT = "." + OUTDIR + "data/mixed_tiers.csv"
-
-frame_ells = np.loadtxt(ELLS, delimiter=',', skiprows=1)
-frame_t1 = np.loadtxt(TIER_1, delimiter=',', skiprows=1)
-frame_t2 = np.loadtxt(TIER_2, delimiter=',', skiprows=1)
-frame_t3 = np.loadtxt(TIER_3, delimiter=',', skiprows=1)
-frame_t4 = np.loadtxt(TIER_4, delimiter=',', skiprows=1)
-
-frame_t1 = np.column_stack((frame_t1, \
-	np.full(np.shape(frame_t1)[0], 1)))
-	#np.full(np.shape(frame_t1)[0], 1), \
-		#(frame_ells[np.searchsorted(frame_ells[:, 0], frame_t1[:, 0]), 6]*2)))
-frame_t2 = np.column_stack((frame_t2, \
-	np.full(np.shape(frame_t2)[0], 2)))
-	#np.full(np.shape(frame_t2)[0], 2), \
-		#(frame_ells[np.searchsorted(frame_ells[:, 0], frame_t2[:, 0]), 6]*2)))
-frame_t3 = np.column_stack((frame_t3, \
-	np.full(np.shape(frame_t3)[0], 3)))
-	#np.full(np.shape(frame_t3)[0], 3), \
-		#(frame_ells[np.searchsorted(frame_ells[:, 0], frame_t3[:, 0]), 6]*2)))
-frame_t4 = np.column_stack((frame_t4, \
-	np.full(np.shape(frame_t4)[0], 4)))
-	#np.full(np.shape(frame_t4)[0], 4), \
-		#(frame_ells[np.searchsorted(frame_ells[:, 0], frame_t4[:, 0]), 6]*2)))
-
-frame_all = np.vstack((frame_t1, frame_t3, frame_t4))
-frame_all = frame_all[frame_all[:, 0].argsort()]
-
-np.savetxt(OUTPUT, frame_all, delimiter=',', fmt=['%d', '%f', '%f', '%f', '%d'])
+main()
