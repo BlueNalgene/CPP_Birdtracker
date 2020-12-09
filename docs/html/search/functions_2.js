9800b33ca932344bf52a1dc0db7092f24062a135
Wesley T. Honeycutt <wessubba8888@yahoo.com>
2020-12-09 13:09:48 -0600

    documentation update

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['check_5fpositive',['check_positive',['../namespacefog__removal.html#a7ea01fc3bbbb26aa0f4d20724849308c',1,'fog_removal']]],
   ['concat_5ftiers',['concat_tiers',['../frame__extraction_8cpp.html#a579a6e5cf9846789c07a88e72ef4a7a7',1,'concat_tiers():&#160;frame_extraction.cpp'],['../frame__extraction_8hpp.html#a579a6e5cf9846789c07a88e72ef4a7a7',1,'concat_tiers():&#160;frame_extraction.hpp']]],
   ['contours_5fonly',['contours_only',['../frame__extraction_8cpp.html#a5a1b727e663ec81191fd584338c88227',1,'contours_only(Mat in_frame):&#160;frame_extraction.cpp'],['../frame__extraction_8hpp.html#a5a1b727e663ec81191fd584338c88227',1,'contours_only(Mat in_frame):&#160;frame_extraction.hpp']]],
   ['corner_5fmatching',['corner_matching',['../frame__extraction_8cpp.html#a19b6b938855c1060791509f406645605',1,'corner_matching(Mat in_frame, vector&lt; Point &gt; contour, int plusx, int plusy):&#160;frame_extraction.cpp'],['../frame__extraction_8hpp.html#a19b6b938855c1060791509f406645605',1,'corner_matching(Mat in_frame, vector&lt; Point &gt; contour, int plusx, int plusy):&#160;frame_extraction.hpp']]],
